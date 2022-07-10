82d852ab3468f167549d9ab1514182e7863544a7
YonV <32718226+YonVclaw@users.noreply.github.com>
2022-07-09 20:14:54 -0600

    1

---
+++ FTX/183rd_Air_Assault_FTX.Altis/script_component.hpp
@@ -0,0 +1,15 @@
+#define PREFIX OETA
+#define COMPONENT FTX
+
+// Version
+#define MAJOR 3
+#define MINOR 6
+#define PATCHLVL 0
+
+// Map
+#define MAP Stratis
+
+#define DEBUG_SYNCHRONOUS
+//#define DEBUG_MODE_FULL
+
+#include "\x\cba\addons\main\script_macros_mission.hpp"
